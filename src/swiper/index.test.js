e9c55e22f6669eec02b9267545dfb7a00742f52e
Gabriel Santos <gabriielbs@gmail.com>
2018-07-07 19:49:37 -0300

    added enzyme and startin to write tests

--- src/swiper/index.test.js
+++ src/swiper/index.test.js
@@ -1,5 +1,41 @@
+import React from 'react'
 import ComponentSwiper from './index'
+import { shallow, mount } from 'enzyme'
+
+const swiper = (
+  <ComponentSwiper data={[
+    <div />,
+    <div />,
+    <div />
+  ]} />
+)
 
 test('should render without errors', () => {
-  expect(true)
+  shallow(swiper)
+})
+
+describe('Testing the elements of navigation', () => {
+  const wrapper = mount(
+    <ComponentSwiper
+      data={[
+        <div />,
+        <div />,
+        <div />
+      ]}
+      arrowLeft={(state) => (
+        <a href="#" onClick={() => state.goPrev()}>goNext</a>
+      )}
+      arrowRight={(state) => (
+        <button onClick={() => state.goNext()}>goNext</button>
+      )}
+    />
+  )
+
+  test('the next element should has a button', () => {
+    expect(wrapper.find('.c-swippernext button').length).toBe(1)
+  })
+
+  test('the prev element should has a anchor', () => {
+    expect(wrapper.find('.c-swipperprev a').length).toBe(1)
+  })
 })
